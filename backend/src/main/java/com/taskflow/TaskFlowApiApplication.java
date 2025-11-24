5cded7c81baaa82397612bfc6eadda006f98e755
Eduardo Sato <esatoverdecampo@gmail.com>
2025-11-24 14:25:39 -0300

    Permitir execucao sem arquivo .env

--- backend/src/main/java/com/taskflow/TaskFlowApiApplication.java
+++ backend/src/main/java/com/taskflow/TaskFlowApiApplication.java
@@ -2,16 +2,16 @@ package com.taskflow;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv; // Importe isso
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class TaskFlowApiApplication {
 
     public static void main(String[] args) {
         
-        // Adicione este bloco ANTES do SpringApplication.run
-        // Ele carrega o .env e joga as variáveis para o sistema (System Properties)
-        Dotenv dotenv = Dotenv.configure().load();
+        Dotenv dotenv = Dotenv.configure()
+                              .ignoreIfMissing()
+                              .load();
         dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
         SpringApplication.run(TaskFlowApiApplication.class, args);
